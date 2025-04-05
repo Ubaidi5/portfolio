a4017a41ffe786ba3772890bacac14ce871b3b65
ubaidi5 <ubaid7625@gmail.com>
2025-04-05 15:33:05 +0500

    add redirect for blog page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/blog/:path*",
+        destination: "/blogs/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
