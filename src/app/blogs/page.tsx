4cd78aaac42ef1889b4cdc8eaff70478905ee971
ubaidi5 <ubaid7625@gmail.com>
2025-04-06 12:34:24 +0500

    add contentfull

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,4 +1,8 @@
-export default function Page() {
+import { getBlogPosts } from "@/lib/contentful";
+
+export default async function Page() {
+  const blogs = await getBlogPosts();
+
   return (
     <div>
       <h1>This is a blog page</h1>
---
+++ src/lib/contentful.ts
@@ -0,0 +1,15 @@
+import { createClient } from "contentful";
+
+export const createContentClient = () => {
+  return createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || "",
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+  });
+};
+const client = createContentClient();
+
+export const getBlogPosts = async () => {
+  const results = await client.getEntries({ content_type: "pageBlogPost" });
+  const blogPosts = results.items.map((blog) => blog.fields);
+  return blogPosts;
+};
