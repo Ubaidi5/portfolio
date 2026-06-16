24f9618acf17f7b769f8f819edc7320efab1fa1a
ubaidi5 <ubaid7625@gmail.com>
2026-06-17 01:50:27 +0500

    Update base URL in robots.ts to reflect the correct domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,8 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl =
-    process.env.NEXT_PUBLIC_BASE_URL || "https://ubaidhussain.com";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://ubaidhussain.me";
 
   return {
     rules: {
